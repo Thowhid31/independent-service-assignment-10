be9761ef0cf9825e1ad3eed175d83fb9f2f54db7
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 00:37:42 +0600

    added nav photo

--- src/Pages/SharedPages/Header/Header.js
+++ src/Pages/SharedPages/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Header.css'
+
+import navImage from '../../../images/HeadBanner/travel-guid.png'
 
 const Header = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar className='nav-class' bg="" variant="">
+                <Container >
+                    <Navbar.Brand className='nav-img' href="#home"><img src={navImage} alt="" /></Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link className='nav-link-color' href="#home">Home</Nav.Link>
+                        <Nav.Link className='nav-link-color' href="#features">Features</Nav.Link>
+                        <Nav.Link className='nav-link-color' href="#pricing">Pricing</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
