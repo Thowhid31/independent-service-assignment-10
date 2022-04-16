8d6a6312914225cf92bbf7ef4e902cf9412f0fe5
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 01:40:44 +0600

    update hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/SharedPages/Footer/Footer';
 import Header from './Pages/SharedPages/Header/Header';
@@ -11,7 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
+      
       <Footer></Footer>
     </div>
   );
