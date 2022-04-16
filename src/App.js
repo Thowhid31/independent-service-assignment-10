b92c8949918fa2e61ee466ec56d5724546f34ccd
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 02:53:09 +0600

    implement just login signup components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
 import MyServiceDetail from './Pages/MyServiceDetail/MyServiceDetail';
 import Footer from './Pages/SharedPages/Footer/Footer';
 import Header from './Pages/SharedPages/Header/Header';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<MyServiceDetail></MyServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       
