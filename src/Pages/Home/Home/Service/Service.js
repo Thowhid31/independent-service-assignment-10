1c981c49c1739b65bc42ded0b2a1c6d6d086606b
Thowhid31 <thowhidantor16@gmail.com>
2022-04-16 22:53:00 +0600

    implement the responsive for website

--- src/Pages/Home/Home/Service/Service.js
+++ src/Pages/Home/Home/Service/Service.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({service}) => {
     const {name, img, description, price} = service;
     return (
-        <div>
+        <div className='single-service'>
             <img src={img} alt="" />            
             <h3>{name} </h3>
             <p>Cost: {price}</p>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Home/Service/Service';
+import './Services.css';
 
 const Services = () => {
 
@@ -12,14 +13,16 @@ const Services = () => {
     }, [])
 
     return (
-        <div>
-            <h1>My Services</h1>
+        <div> 
+            <h1 className='services-header'>My Services</h1>
+            <div className='services-container'>
             {
                 services.map(service => <Service
                 key={service.id}
                 service = {service}
                 ></Service>)
             }
+            </div>
         </div>
     );
 };
