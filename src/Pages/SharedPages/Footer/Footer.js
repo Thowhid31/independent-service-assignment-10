bae27af1c5c97b55841eb8e7b0a13e539be209bc
Thowhid31 <thowhidantor16@gmail.com>
2022-04-18 00:51:32 +0600

    added social  icon with link

--- src/Pages/SharedPages/Footer/Footer.js
+++ src/Pages/SharedPages/Footer/Footer.js
@@ -1,11 +1,29 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Footer.css'
+import fb from '../../../images/HeadBanner/Facebook_Logo.png'
+import li from '../../../images/HeadBanner/linkedIn.png'
+import tr from '../../../images/HeadBanner/twitter-thumbnail.png'
+import yt from '../../../images/HeadBanner/y.png'
+
 
 const Footer = () => {
     return (
         <footer className='mt-5 footer-class'>
+            <div>
+                <h5>Connect with Me</h5>
+                {/* <p><FontAwesomeIcon icon="fa-brands fa-facebook" /></p> */}
+                <div>
+                    <p className='social-icon-class'>
+                        <a href="https://www.facebook.com/thowhid.antor/"><img src={fb} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/thowhidantor/"><img src={li} alt="" /></a>
+                        <a href="https://twitter.com/home"><img src={tr} alt="" /></a>
+                        <a href="https://www.youtube.com/"><img src={yt} alt="" /></a>
+                    </p>
+                </div>
             <p><small><span dangerouslySetInnerHTML={{ "__html": "&copy;"} }/> 
                  Copyright: {new Date().getFullYear()}</small></p>
+            </div>
         </footer>
     );
 };
