038d9c594f2fff997c275d2ded368430b150b4b0
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 17:40:44 +0600

    verification done

--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import SocialLogin from '../Login/SocialLogin/SocialLogin';
+
+
 
 
 const Signup = () => {
@@ -14,7 +16,8 @@ const Signup = () => {
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification:(true)});
+      const [updateProfile, updating, errorUpdate] = useUpdateProfile(auth);
 
     const navigate = useNavigate();
 
@@ -22,16 +25,20 @@ const Signup = () => {
         navigate('/login')
     }
 
-    const handleSignup = event => {
+    const handleSignup = async event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
         
 
-        if(termsAgree){
-            createUserWithEmailAndPassword(email, password);
-        }
+        // if(termsAgree){
+
+           await createUserWithEmailAndPassword(email, password);
+           await updateProfile({displayName : name})
+           alert('Your Profile Updated')
+           navigate('/home')
+        // }
 
         
 
