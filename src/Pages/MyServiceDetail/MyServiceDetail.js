1d29524356ed1b91ce759f8034567e8ea60897de
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 22:27:24 +0600

    final touch with resposive confirm page

--- src/Pages/MyServiceDetail/MyServiceDetail.js
+++ src/Pages/MyServiceDetail/MyServiceDetail.js
@@ -6,23 +6,24 @@ import confirmPhoto from '../../images/others/confirm.jpg'
 const MyServiceDetail = () => {
     const { serviceId } = useParams()
     return (
-        <div className='container col-lg-12 col-md-6'>
+        <div className='container'>
             <div>
                 <h1 className='mt-3' style={{ color: 'tomato' }}>My service: Id — {serviceId}</h1>
                 <p>(Read Condition and all Criteria which provide my service which is vary with my service id. So read properly.)</p>
             </div>
-            <div className='confirm-element-div'>
+            <div className='section-confirm'>
+                {/* <img className='confirm-photo' src={confirmPhoto} alt="" /> */}
                 <img className='confirm-photo' src={confirmPhoto} alt="" />
-                <div className='section-confirm container mt-3'>
+                <div className=' mt-3'>
                     <p>Conditions: If you confirm your Book, then refund policy should be maintain. You can cancel your booking, but in refund your booking amount fall day by day in front of Tour Day. </p>
                     <p>To know About me: Click 'About' menu.</p>
-                    <h4>Are you Confirm to book now? Then Click button bellow!</h4>
+                    <h4>Are you Confirm to book now? <br/> Then Click button bellow!</h4>
                 </div>
 
             </div>
             <div>
                 <Link to='/checkout'>
-                    <button className='btn btn-primary check-btn'>Confirm Book</button>
+                    <button className='btn btn-primary check-btn mt-3'>Confirm Book</button>
                 </Link>
             </div>
         </div>
