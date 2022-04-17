0aae774d5ae2279d560ea89de970f6bb476c50a4
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 23:02:42 +0600

    added thank division completed

--- src/Pages/Checkout/ThankYou/ThankYou.js
+++ src/Pages/Checkout/ThankYou/ThankYou.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import thankYou from '../../../images/others/Thank_you_card_with_rose_flower_watercolor.jpg'
+import './ThankYou.css'
 
 const ThankYou = () => {
     return (
         <div>
-            <h1>Thank You I accept Your Booking.</h1>
+            <h1 className='thank-h1'>CONGRATULATION!<br/> I accept Your Booking.</h1>
+            <img className='thank-photo' src={thankYou} alt="" />
         </div>
     );
 };
