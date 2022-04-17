0a8bc4d3e631176d743f53504b29e9b2d4afe68d
Thowhid31 <thowhidantor16@gmail.com>
2022-04-17 16:28:34 +0600

    implement checkout for terms and condition

--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css'
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
@@ -7,6 +7,7 @@ import SocialLogin from '../Login/SocialLogin/SocialLogin';
 
 
 const Signup = () => {
+    const [termsAgree, setTermsAgree] = useState(false)
 
     const [
         createUserWithEmailAndPassword,
@@ -26,8 +27,13 @@ const Signup = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        
 
-        createUserWithEmailAndPassword(email, password);
+        if(termsAgree){
+            createUserWithEmailAndPassword(email, password);
+        }
+
+        
 
     }
 
@@ -41,9 +47,9 @@ const Signup = () => {
                 <input type="text" name="" id="" placeholder='Your Name'/>
                 <input type="email" name="email" id="" placeholder='Your Email' required />
                 <input type="password" name="password" id="" placeholder='Password' required/>
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept all cost and conditions! </label>
-                <input className='btn btn-info' type="submit" value='Signup'/>
+                <input onClick={()=> setTermsAgree(!termsAgree)} className='mx-2' type="checkbox" name="terms" id="terms" />
+                <label className={termsAgree ? 'text-dark' : 'text-danger'} htmlFor="terms">Accept all cost and conditions! </label>
+                <input disabled={!termsAgree} className='btn btn-info mt-2 mb-2' type="submit" value='Signup'/>
             </form>
             <p>Already in My site? <Link to='/login' className='signup-toggle text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             <SocialLogin></SocialLogin>
